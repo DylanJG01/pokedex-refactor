e1076e01a0207d63c9730e537dfbd95296c80926
root <root@Endiness.localdomain>
2023-04-06 09:25:48 -0700

    second commit

---
+++ pokedex-refactor/flask-backend/app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+# from .routes import simple
+from .config import Configuration
+from .models import db
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+
+app = Flask(__name__)
+
+app.config.from_object(Configuration)
+
+#app.register_blueprint(.bp)
+
+db.init_app(app)
+
+Migrate(app,db)
+# import statement for CSRF
+
+
+# after request code for CSRF token injection
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie(
+        'csrf_token',
+        generate_csrf(),
+        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
+        samesite='Strict' if os.environ.get(
+            'FLASK_ENV') == 'production' else None,
+        httponly=True)
+    return response
